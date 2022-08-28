d8f424dc51ce9a374d470e75a8eed7849e43565d
parksu00 <youi1213@naver.com>
2022-08-28 16:22:53 +0900

    data save

--- src/components/LineButton.js
+++ src/components/LineButton.js
@@ -5,7 +5,7 @@ import PropsTypes from 'prop-types';
 
 const StyledTouchableOpacity = styled.TouchableOpacity`
     background-color: lightgreen;
-    width:150%;
+    width:200%;
     align-items:center;
 `;
 
